d3dbf6e2d8b854f0fb74787e9deb58c44c88d53e
Andrey Meshkov <am@adguard.com>
2025-03-26 18:58:05 +0300

    Use GH actions to build filters

--- scripts/auto_build.sh
+++ scripts/auto_build.sh
@@ -11,8 +11,8 @@ set -x -e
 # NOTE: id 12 is not included because it is an obsolete Safari filter.
 ADGUARD_FILTERS="1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23,24,224"
 
-# Default mode is "all"
-MODE="all"
+# Default mode is "3p"
+MODE="3p"
 
 # Define paths for wildcard domain expansion
 CHROMIUM_MV3_PATH="./platforms/extension/chromium-mv3"
@@ -25,10 +25,10 @@ while [[ $# -gt 0 ]]; do
     case "$1" in
         --mode)
             shift
-            if [[ "$1" == "all" || "$1" == "adguard" ]]; then
+            if [[ "$1" == "3p" || "$1" == "adguard" ]]; then
                 MODE="$1"
             else
-                echo "Invalid mode. Use 'all' or 'adguard' as the mode."
+                echo "Invalid mode. Use '3p' or 'adguard' as the mode."
                 exit 1
             fi
             shift
@@ -44,8 +44,8 @@ done
 echo "Selected mode: $MODE"
 
 # Depending on the mode, execute different commands
-if [[ "$MODE" == "all" ]]; then
-    # Build all filters except ours to keep 1 hour update cycle of patches
+if [[ "$MODE" == "3p" ]]; then
+    # Build third-party filters except ours to keep 1 hour update cycle of patches
     # for our filters.
     yarn build --skip=$ADGUARD_FILTERS --report='report-third-party.txt'
 
@@ -71,6 +71,3 @@ fi
 
 # Validate platforms and locales
 yarn validate
-
-# Update built platforms and filters in the repository
-yarn push
